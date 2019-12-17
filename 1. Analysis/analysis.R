34135ca7a424553e09f593fe50bedb22a9913670
Lærke Mygind <laerke.mygind.groenfeldt@regionh.dk>
2019-12-17 12:25:25 +1100

    updated README.md

---
+++ 1. Analysis/analysis.R
@@ -0,0 +1,20 @@
+#Import data from csv.
+
+Study.2.MASTER <- read.csv("D:/Work backup/1. Projects/PhD/Study 2/Data/Study 2 MASTER.csv", header=FALSE)
+View(Study.2.MASTER)
+
+#Defining variables. 
+
+#Testing distribution of data.
+
+#Testing for attrition and randomness of missing data.
+
+#Testing hypothesis 1.
+
+#Testing hypothesis 1a.
+
+#Testing hypothesis 2.
+
+#Testing hypothesis 3.
+
+#Sensitivity analyses.
